406ee264315f65e005b1c84f09dd5fcac4c1f6f7
NGELI NSABAKA <rickoungeli@gmail.com>
2021-07-11 16:11:29 +0200

    Les routes GET /api/sauces, POST /api/sauces et GET /api/sauces/:id fonctionnent

--- controllers/sauces.controllers.js
+++ controllers/sauces.controllers.js
@@ -5,36 +5,35 @@ const sauceSchema = require('../models/sauces.modele')
 
 //Fonction pour créer une sauce (méthode POST sur la route /api/sauces)
 exports.creerSauce = (req, res, next) => { 
+    //Ces 3 lignes sont à effacer
     console.log ("Demande de création de sauce")  
-    const sauceObject = JSON.parse(req.body.sauce)
-    //delete sauceObject._id
+    console.log("Voici les paramètres de la requête : " + req.params)
+    if(req.params.userId) {console.log("Voici l'ID de l'utilisateur : " +req.params.userId)} else {console.log("L'ID de l'utilisateur n'est pas envoyée dans la reqûete")}
+
+
+    const sauceObject = JSON.parse(req.body.sauce)    
     const sauce = new sauceSchema({
-        //...sauceObject,
-        name: sauceObject.name,
-        manufacturer: sauceObject.manufacturer,
-        description: sauceObject.description,
-        mainPepper: sauceObject.mainPepper,
+        ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        heat: sauceObject.heat,
-        likes: [1],
-        dislikes: [0],
-        usersLiked: ["60e885ec2d53891cb43a9394"],
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
         usersDisliked: [],        
-        userId: "60e885ec2d53891cb43a9394"
-        
+        userId: "60e885ec2d53891cb43a9394"  //Où dois-je trouver cette valleur?
     })
-    
     sauce.save()
     .then(()=> res.status(201).json({ 'message': 'Sauce enregistrée'}))
-    .catch(error => res.status(400).json({ error }))
-
+    .catch(error => {
+        console.log("voici l'erreur : " + error)
+        res.status(400).json({ error })
+    })
 }
 
 //Fonction pour renvoyer toutes les sauces au frontend
 exports.renvoyerToutesLesSauces = (req, res, next)=>{ 
     console.log ("Demande liste de sauces")
     sauceSchema.find()
-    .then( sauces =>res.status(200).json({ sauces }) )
+    .then( sauces =>res.status(200).send(sauces) )
     .catch(error => res.status(400).json({ error }))
 }
 
@@ -42,7 +41,7 @@ exports.renvoyerToutesLesSauces = (req, res, next)=>{
 exports.renvoyerSauce = (req, res, next)=>{
     console.log ("Demande d'une sauce")
     sauceSchema.findOne({_id: req.params.id})
-    .then( sauce =>res.status(200).json({ sauce }) )
+    .then( sauce =>res.status(200).send(sauce) )
     .catch(error => res.status(404).json({ error }))
 }
 
